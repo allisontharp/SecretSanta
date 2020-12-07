f7ea36e914a8fbaebb0e82bb007662d3cadf90c2
Allison <junk+github@techtrek.io>
2020-12-07 12:58:17 -0500

    matching logic is a little better

--- backend/SecretSantaFuncs.py
+++ backend/SecretSantaFuncs.py
@@ -1,17 +1,18 @@
 import random
 import json
+import smtplib, ssl
+from config import * 
 
-def generateSecretSantas(group):
+def generateSecretSantas(group, tryNumber):
     households = [h['houseHolds'] for h in group if h['userName'] == 'General'][0]
     participants = [p for p in group if p['userName'] != 'General']
     matchDictionary = {} # santa: child
     participantsWithoutSanta = participants.copy()
-    maxTries = 3
-    tryNumber = 0
+    maxTries = 10
     validMatches = True
-    isComplete = False
 
-    while validMatches == True:
+    if (tryNumber <= maxTries):
+        print(tryNumber)
         matchDictionary = {} # santa: child
         participantsWithoutSanta = participants.copy()
         random.shuffle(participants)
@@ -28,23 +29,31 @@ def generateSecretSantas(group):
                 for h in householdParticipants:
                     possibleMatches = [p for p in possibleMatches if p['userName'] != h]
             # Should probably check if there are no matches here..
-            if(len(possibleMatches) == 0 and tryNumber < maxTries):
-                tryNumber += 1
-            elif (len(possibleMatches) > 0):
+            if (len(possibleMatches) > 0):
                 # Save the match
                 match = random.choice(possibleMatches)
                 matchDictionary[participantName] = match['jsonObject']
                 participantsWithoutSanta = [p for p in participantsWithoutSanta if p['userName'] != match['userName']]
-            else:
-                validMatches = False
-
+            
             if(validMatches == True and p == participants[-1]):
                 validMatches = False
-                isComplete = True
                 continue
-    if isComplete:
+    if len(matchDictionary) == len(participants):
         return matchDictionary
+    else:
+        tryNumber += 1
+        generateSecretSantas(group, tryNumber)
 
 
-def sendEmail(sender,recipient,participantPreferences):
-   
+def sendEmail(recipient,message):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = emailVariable
+    receiver_email = recipient
+    password = passwordVariable
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
--- backend/handler.py
+++ backend/handler.py
@@ -2,6 +2,9 @@ import json
 from pprint import pprint
 from DynamoFuncs import *
 from SecretSantaFuncs import *
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import re
 
 def getGroup(event, context):
     r = getItemsFromTable('secretSanta', Key('groupName').eq(event['pathParameters']['groupName']))
@@ -87,10 +90,91 @@ def updateHouseholds(event, context):
 def generateMatches(event, context):
     group = json.loads(event['body'])
     print(group)
+    participants = [p for p in group if p['userName'] != 'General']
     # Generate matches 
-    matches = generateSecretSantas(group)
+    matches = generateSecretSantas(group, 0)
+    if(len(participants) != len(matches)):
+        raise ValueError('Made {matchCount} matches but had {partCount} participants.  Matches: {matches}'.format(matchCount = len(matches), partCount = len(participants), matches = matches))
 
     # Send Emails    
+    groupRules = [p for p in group if p['userName'] == 'General'][0]
+    groupJson = json.loads(groupRules['jsonObject'])
+    print('matches:')
+    print(matches)
+    for m in matches:
+        j = json.loads(matches[m])
+        santa = [p for p in group if p['userName'] == m][0]
+        santaEmail = json.loads(santa['jsonObject'])['participantEmail']
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Secret Santa"
+        message["From"] = emailVariable
+        message["To"] = santaEmail
+
+        participantColor = ''
+        participantFood = ''
+        participantTeam = ''
+        participantScent = ''
+        participantStore = ''
+        participantGadget = ''
+        participantEnough = ''
+        participantEnjoy= ''
+        participantMisc = ''
+        if('participantColor' in j):
+            participantColor = '<b>Favorite Color:</b> {color} \n<br>'.format(color = j['participantColor'])
+        if('participantFood' in j):
+            participantFood = '<b>Favorite food/candy/snack:</b> {food} \n<br>'.format(food = j['participantFood'])
+        if('participantTeam' in j):
+            participantTeam = '<b>Favorite team(s) and/or fictional character(s):</b> {val} \n<br>'.format(val = j['participantTeam'])
+        if('participantScent' in j):
+            participantScent = '<b>Favorite scent for the home:</b> {val} \n<br>'.format(val = j['participantScent'])
+        if('participantStore' in j):
+            participantStore = '<b>Stores they love just about everything:</b> {val} \n<br>'.format(val = j['participantStore'])
+        if('participantGadget' in j):
+            participantGadget = '<b>Gadgets or tools they want:</b> {val} \n<br>'.format(val = j['participantGadget'])
+        if('participantEnough' in j):
+            participantEnough = '<b>They have enough:</b>{val} \n<br>'.format(val = j['participantEnough'])
+        if('participantEnjoy' in j):
+            participantEnjoy = '<b>They think they would really enjoy:</b> {val} \n<br>'.format(val = j['participantEnjoy'])
+        if('participantMisc' in j):
+            participantMisc = '<b>Anything else:</b> {val} \n<br>'.format(val = j['participantMisc'])
+        text = f"""Hey {m}! \n
+You are {j['participantName']}'s Secret Santa!  Here are their responses: \n<br>
+{re.sub('<[^>]*>','',participantColor)}
+{re.sub('<[^>]*>','',participantFood)}  
+{re.sub('<[^>]*>','',participantTeam)}   
+{re.sub('<[^>]*>','',participantScent)}  
+{re.sub('<[^>]*>','',participantStore)}
+{re.sub('<[^>]*>','',participantGadget)}
+{re.sub('<[^>]*>','',participantEnough)}
+{re.sub('<[^>]*>','',participantEnjoy)}
+{re.sub('<[^>]*>','',participantMisc)}
+Remember, we are planning on trading gifts on {groupJson['groupDeadline']}.  We are trying to spend between ${groupJson['dollarMinimum']} and ${groupJson['dollarMaximum']}.
+{groupJson['groupRules']}."""
+
+        html = f"""<html><body>
+Hey {m}! <br>\n
+You are <b>{j['participantName']}'s</b> Secret Santa!  Here are their responses: <br>\n
+{participantColor}
+{participantFood} 
+{participantTeam} 
+{participantScent} 
+{participantStore}
+{participantGadget}
+{participantEnough}
+{participantEnjoy}
+{participantMisc}
+Remember, we are planning on trading gifts on {groupJson['groupDeadline']}.  We are trying to spend between ${groupJson['dollarMinimum']} and ${groupJson['dollarMaximum']}.
+{groupJson['groupRules']}.
+</body></html>"""   
+
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        message.attach(part1)
+        message.attach(part2)
+
+        sendEmail(santaEmail,message)
 
     # Send Response
     body = {
@@ -103,6 +187,5 @@ def generateMatches(event, context):
         "body": json.dumps(body)
     }
 
-    r = sendEmail('allison.tharp@gmail.com','allison@techtrek.io','')
-    print(r)
+    
     return response
