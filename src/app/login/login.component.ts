377fcbf4cc1ce873e673892ca68e15e3a86939d9
allisontharp <junk+github@techtrek.io>
2021-04-04 12:13:36 -0400

    login mocked

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  incorrectGuid: boolean;
+  accountGuid: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(): void{
+    console.log(this.accountGuid)
+    if(this.accountGuid == "914ca442-ce7d-4a96-ad8c-1a99c4989cdc"){
+      this.incorrectGuid = false;
+      window.location.href = '/groups';
+    } else {
+      this.incorrectGuid = true;
+    }
   }
 
 }
